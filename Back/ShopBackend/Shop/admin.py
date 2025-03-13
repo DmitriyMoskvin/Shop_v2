6b8cdae5f7bbd2965d1542c797c99a726b881ecc
Dmitriy Moskvin <dvashnapsa@gmail.com>
2025-03-13 12:41:38 +0500

    feat(Shop/Admin): Customize Product, Sections and Categories pages

--- Back/ShopBackend/Shop/admin.py
+++ Back/ShopBackend/Shop/admin.py
@@ -1,10 +1,43 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import Product, Sizes, Sections, Categories, Colors, ProductImage, Item
 
-admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('photo_tag', 'name', 'description', 'price_per_unit', 'section', 'category', 'isPublished', 'get_sizes', 'get_colors')
+    list_editable = ('price_per_unit', 'isPublished')
+    list_per_page = 10
+    search_fields = ('name',)
+    list_filter = ('isPublished',)
+
+    filter_horizontal = ('size', 'colors')
+
+    @admin.display(description='Размеры')
+    def get_sizes(self, obj):
+        return [size.name for size in obj.size.all()]
+
+    @admin.display(description='Цвета')
+    def get_colors(self, obj):
+        return [color.name for color in obj.colors.all()]
+
+    @admin.display(description='Фото')
+    def photo_tag(self, obj):
+        return format_html(f'<img src="{obj.photo.url}" style="width: 50px;" />')
+
+@admin.register(Sections)
+class SectionsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'isPublished')
+    list_editable = ('isPublished', )
+
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'isPublished')
+    list_editable = ('isPublished', )
+
+
 admin.site.register(Sizes)
-admin.site.register(Sections)
-admin.site.register(Categories)
 admin.site.register(Colors)
 admin.site.register(ProductImage)
 admin.site.register(Item)
